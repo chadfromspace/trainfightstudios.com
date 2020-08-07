c4cd13b9b9985d45d8396e0ed49d1d57ec3d8f3f
imac <chadmdubay@gmail.com>
2020-08-07 00:33:36 -0400

    Updates

---
+++ precache-manifest.9eec3bd9e5c1a3a81563a2bde11de6ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf4b8dd92fec1c208fa50e2e37b9582",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b435d7ee77e2f232d05d",
+    "url": "/Portfolio/static/css/main.64ceaab3.chunk.css"
+  },
+  {
+    "revision": "87dba2e0a9aad72e9520",
+    "url": "/Portfolio/static/js/2.50030d24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.50030d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b435d7ee77e2f232d05d",
+    "url": "/Portfolio/static/js/main.0c410c6b.chunk.js"
+  },
+  {
+    "revision": "728a212b786971eea992",
+    "url": "/Portfolio/static/js/runtime-main.f73464aa.js"
+  },
+  {
+    "revision": "b3438e843f84f388467b32366fab9acf",
+    "url": "/Portfolio/static/media/aboutme.b3438e84.png"
+  }
+]);
