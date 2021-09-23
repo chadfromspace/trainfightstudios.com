bd3f2bd7fe85a02a08e332712d300868ded47221
imac <chadmdubay@gmail.com>
2021-09-23 19:19:54 -0400

    Update.

---
+++ build/precache-manifest.1ed7655a8bcbec7643b60a417ae5caf7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe27771aad93cd4ba8456d4323558d44",
+    "url": "/trainfightstudios.com/index.html"
+  },
+  {
+    "revision": "206e17a0c16d657ebd86",
+    "url": "/trainfightstudios.com/static/css/main.021ce75e.chunk.css"
+  },
+  {
+    "revision": "4e2c6ee5824784f01607",
+    "url": "/trainfightstudios.com/static/js/2.dbaba28e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/trainfightstudios.com/static/js/2.dbaba28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206e17a0c16d657ebd86",
+    "url": "/trainfightstudios.com/static/js/main.10dbee7b.chunk.js"
+  },
+  {
+    "revision": "25f0d8c1b752cd2c8e8b",
+    "url": "/trainfightstudios.com/static/js/runtime-main.21f84a1c.js"
+  },
+  {
+    "revision": "3b800f04c49f5e2dda676f279aa2fe77",
+    "url": "/trainfightstudios.com/static/media/Portfolio.3b800f04.gif"
+  },
+  {
+    "revision": "b7de0e48a8dd5e07e0a760fdf8428e2f",
+    "url": "/trainfightstudios.com/static/media/SpacetimeVideoGame.b7de0e48.png"
+  },
+  {
+    "revision": "57c6ba40032e1badc72b4f79abc1d31e",
+    "url": "/trainfightstudios.com/static/media/TrainfightStudiosLogo.57c6ba40.png"
+  },
+  {
+    "revision": "43376d70b045767878ede5ae42afa225",
+    "url": "/trainfightstudios.com/static/media/tbof.43376d70.png"
+  }
+]);
